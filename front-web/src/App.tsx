a982c75155b5437a1ad7757db4660b0a70ae1ddb
Dario <darioo_dario@hotmail.com>
2021-01-09 14:37:24 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      <Home/>      
     </>
   );
 }
