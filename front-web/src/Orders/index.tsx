aed33e26685f346bc8e6123ca08ea2751c695528
Dario <darioo_dario@hotmail.com>
2021-01-09 19:32:08 -0300

    Final da primeira aula front-end

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
 
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([])
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+
+    }, []);
+
     return (
         <div className="orders-container">
-            <StepsHeader/>
+            <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
